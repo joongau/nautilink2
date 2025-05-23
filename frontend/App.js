1c9b174b442d9249e73e8b657af30c47228de2bb
Jonathan <gaultier.jonathan@gmail.com>
2025-05-23 19:39:57 +0200

    to delete

--- frontend/App.js
+++ frontend/App.js
@@ -1,3 +1,5 @@
+import 'react-native-gesture-handler';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import MapScreen from './screens/MapScreen';
 
@@ -16,17 +18,19 @@ const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Splash">
-        <Stack.Screen name="Splash" component={SplashScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="Register" component={RegisterScreen} />
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Profile" component={ProfileScreen} />
-        <Stack.Screen name="Map" component={MapScreen} />
-        <Stack.Screen name="AlertTypeScreen" component={AlertTypeScreen} options={{ title: 'Signaler une alerte' }} />
-        <Stack.Screen name="AlertFormScreen" component={AlertFormScreen} options={{ title: 'Détail de l’alerte' }} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Splash">
+          <Stack.Screen name="Splash" component={SplashScreen} options={{ headerShown: false }} />
+          <Stack.Screen name="Login" component={LoginScreen} />
+          <Stack.Screen name="Register" component={RegisterScreen} />
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Profile" component={ProfileScreen} />
+          <Stack.Screen name="Map" component={MapScreen} />
+          <Stack.Screen name="AlertTypeScreen" component={AlertTypeScreen} options={{ title: 'Signaler une alerte' }} />
+          <Stack.Screen name="AlertFormScreen" component={AlertFormScreen} options={{ title: 'Détail de l’alerte' }} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </GestureHandlerRootView>
   );
 }
